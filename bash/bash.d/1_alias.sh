299a90348fcd083993aae7ac470d5faeb10b4c07
Kuang Chen <k.chen@criteo.com>
2019-04-25 16:24:40 -0700

    Add hdfs_quota and yarn_kill aliases.

--- bash/bash.d/1_alias.sh
+++ bash/bash.d/1_alias.sh
@@ -16,3 +16,11 @@ alias hdfs_delete_now="hdfs dfs -rm -r -skipTrash"
 grep_exception() {
     grep -i Exception "$1" | sort | uniq 
 }
+
+yarn_kill() {
+    check_num_args 1 "$@"
+    local -r app_id="$1"
+    yarn application -kill "$app_id"
+}
+
+alias hdfs_quota="hadoop fs -count -q -v"
